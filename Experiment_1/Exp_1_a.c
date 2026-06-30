e31473ab3a3f838382a7b57ebb293d95529df452
SaiGuruInukurthi <srivenkatasaiguru@gmail.com>
2026-06-30 09:15:58 +0530

    add implementation of DFA for string recognition in Exp_1_a.c and include executable file

---
+++ Experiment_1/Exp_1_a.c
@@ -0,0 +1,71 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i;
+    int state = 0;
+    
+    printf("Enter a string  (only a and b):");
+    scanf ("%s", str);
+    
+    for (i=0; str[i] != '\0'; i++){
+        switch(state){
+            case 0:
+                if (str[i] == 'a'){
+                    state = 1;
+                }
+                else if (str[i] == 'b'){
+                    state = 0;
+                }
+                else {
+                    printf("Invalid input");
+                    return 0;
+                }
+            break;
+        case 1:
+                if (str[i] == 'a'){
+                    state = 1;
+                }
+                else if (str[i] == 'b'){
+                    state = 2;
+                }
+                else {
+                    printf("Invalid input");
+                    return 0;
+                }
+            break;
+        case 2:
+                if (str[i] == 'a'){
+                    state = 1;
+                }
+                else if (str[i] == 'b'){
+                    state = 3;
+                }
+                else {
+                    printf("Invalid input");
+                    return 0;
+                }
+            break;
+        case 3:
+                if (str[i] == 'a'){
+                    state = 1;
+                }
+                else if (str[i] == 'b'){
+                    state = 0;
+                }
+                else {
+                    printf("Invalid input");
+                    return 0;
+                }
+            break;
+        }
+    }
+    
+    if (state ==  3){
+        printf("String accepted");
+    }else{
+        printf("String rejected");
+    }
+    return 0;
+}
